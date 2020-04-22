89927d11165462b3034156c0216faad544bd5e1d
Jose antonio Salguero pedros <rollerdock@MacBook-Pro-de-Jose-7.local>
2020-04-22 21:09:51 +0200

    actualixación 22/04/2020

---
+++ Funciones/sumadosnum.py
@@ -0,0 +1,10 @@
+
+a=int(input("Introduce el primer valor"))
+b=int(input("Introduce el segundo valor"))
+
+def suma(a,b):  
+    resultado=a+b
+    return resultado
+
+resultado=suma(a,b)
+print(resultado)
---
+++ Scripts/repeticiones.py
@@ -0,0 +1,5 @@
+import sys
+texto=sys.argv[1]
+repeticiones=int(sys.argv[2])
+for num in range(repeticiones):
+    print(texto)
