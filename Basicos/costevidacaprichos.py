da0f62fc0335e4144d082c0be9e9abdbfb398b30
Rollerdock <jasplleida@gmail.com>
2023-05-01 15:40:41 +0200

    Organización

--- Basicos/costevidacaprichos.py
+++ Basicos/costevidacaprichos.py
@@ -1,6 +1,6 @@
 print ("Hola, vamos a proceder a calcular tu tiempo de vida que te costará si te compras el capricho")
 coste_aparato=float(input("introduce el coste del aparato : "))
-salario=float(2000)
+salario=float(1000)
 precioporhora=float((salario/21)/7.5)
 horas=float(coste_aparato/precioporhora)
 print("El precio por hora es :",precioporhora)
--- oksss.py
+++
@@ -1,6 +0,0 @@
-#este es oks 
-print("hola")
-
-
-#ljljljz<cxljl
-
