1df592af9715beead34e4daa7751f3bfd2106f5f
Rollerdock <jasplleida@gmail.com>
2023-05-12 21:36:28 +0200

    ok

--- Avanzado/POO/Ejemplo_de_clase.py
+++ Avanzado/POO/Ejemplo_de_clase.py
@@ -1,37 +1,55 @@
 
-
 class Coche:
     # Atributos o propiedades (variables)
-    # caracteristicas del coche
-    color = "Rojo"
-    marca = "Ferrari"
-    modelo = "Aventador"
-    velocidad = 300
-    caballaje = 500
-    plazas = 2
-
-
-# Métodos, son acciones que hace el objeto, en (coche) (funciones)
-
+    # caracteristicas del coche de la clase padre
+    ruedas = 4
+    volante = 1
+    
+    def __init__ (self,color,plazas,cv,vel,marca,modelo):
+        self.color = color
+        self.plazas = plazas
+        self.cv = cv
+        self.vel = vel
+        self.marca = marca
+        self.modelo = modelo
 
     def acelerar(self):
-        self.velocidad += 1
-
-    def frenar(self):
-        self.velocidad -= 1
+        self.vel += 1 
 
     def getVelocidad(self):
-        return self.velocidad
-
-# Fin definición clase
+        return self.vel
 
 
-# Crear objetos
-coche = Coche()
+# # Métodos de clase , son acciones que hace la clase padre
 
-print(coche.marca, coche.color, coche.marca)
+# @classmethod
 
-coche.acelerar()
+# def acelerar(cls):
+#     self.velocidad += 1
 
-coche.acelerar()
-print(coche.marca, coche.color, coche.velocidad)
+# def frenar(cls):
+#     self.velocidad -= 1
+
+
+
+# # metodos de instancia
+
+# def getColor(self):
+#     print(coche.color)
+
+
+
+
+# # Crear objetos
+
+coche1 = Coche("rojo","2",300,350,"Lamborgini","Diablo")
+
+print(f"El coche es de la marca {coche1.marca} y modelo {coche1.modelo} \n")
+print(f"Es de color {coche1.color}\n")
+print(f"Tiene {coche1.ruedas} y {coche1.volante} volante\n")
+
+coche1.acelerar()
+coche1.acelerar()
+coche1.acelerar()
+
+print(coche1.getVelocidad())
