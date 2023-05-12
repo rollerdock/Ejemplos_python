264a4bc06ca98f1ea235d1c6052fbc51ef07a850
Rollerdock <jasplleida@gmail.com>
2023-05-12 16:26:55 +0200

    Correcciones

--- Avanzado/subproces.py
+++ Avanzado/subproces.py
@@ -1,13 +1,17 @@
 import subprocess
-subprocess.call('clear')
 
-a=0
-i=0
-z=i
-numcarpetas=int(input("Cuantas carpetas quieres: "))  
-   
-for num in range(0,numcarpetas):
-    
+a = 0
+i = 0
+z = i
+numcarpetas = int(input("¿Cuántas carpetas quieres crear? "))
+
+for num in range(0, numcarpetas):
+    nom = "dir" + str(z)
+    subprocess.call(['mkdir', nom])
+    print(subprocess.check_output('pwd', shell=True))
+    i += 1
+    z = i
+    a += 1
     nom = "dir"+str(z)
     subprocess.run (['cd',nom])       
     subprocess.call (['mkdir',nom])
